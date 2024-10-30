739b9255f6b382037768338977c1024dd1ffe2f2
MdHafizBhuiyan <hafizr2016@gmail.com>
2024-10-31 03:22:01 +0600

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="block">
+  Click Me
+</div>
+</template>
+
+<script>
+export default {
+  props: ['delay']
+}
+
+</script>
+
+<style>
+.block{
+  width:400px;
+  padding: 100px;
+  margin: 40px auto;
+  color: white;
+  background-color: aquamarine;
+  text-align:  center;
+  border-radius: 30px;
+}
+</style>
